32166e154f110fd195fd75fe35580979497ce9f4
unknown <tungtxhe171390@fpt.edu.vn>
2024-10-08 01:01:03 +0700

    commit lai

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -88,4 +88,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-});
+}); 
