e202f61e29c126dccb24aa328d896d7515cff180
Anh179005 <anhnvvhe179005@fpt.edu.vn>
2024-10-14 22:43:14 +0700

    fix

--- Front-end/components/Detailed_post_modal.js
+++ Front-end/components/Detailed_post_modal.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { Modal, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import {
+  Modal,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 const DetailedPostModal = ({ visible, event, onClose }) => {
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+
   if (!event) return null;
+
+  const truncateDescription = (text, maxLength) => {
+    if (text.length <= maxLength) return text;
+    return text.substr(0, maxLength) + "...";
+  };
+
+  const toggleDescription = () => {
+    setIsDescriptionExpanded(!isDescriptionExpanded);
+  };
 
   return (
     <Modal visible={visible} transparent={true} animationType="slide">
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
-
-          <Image source={event.image} style={styles.eventImage} />
-          <Text style={styles.eventTitle}>Title: {event.title}</Text>
-          <Text style={styles.eventGenre}>Genre: {event.genre}</Text>
-          <Text style={styles.eventDate}>Date: {event.date}</Text>
+          <Image source={{ uri: event.image[0] }} style={styles.eventImage} />
+          <Text style={styles.eventDescription}>{event.description}</Text>
+          <TouchableOpacity onPress={toggleDescription}>
+            <Text style={styles.eventDescription}>
+              {isDescriptionExpanded
+                ? event.description
+                : truncateDescription(event.description, 100)}
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.eventDate}>
+            Date: {new Date(event.date).toLocaleDateString()}
+          </Text>
           <Text style={styles.eventLocation}>Location: {event.location}</Text>
 
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
@@ -27,49 +52,50 @@ const DetailedPostModal = ({ visible, event, onClose }) => {
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     margin: 20,
     padding: 20,
     borderRadius: 10,
-    position: 'relative',
+    position: "relative",
   },
   eventImage: {
-    width: '100%',
+    width: "100%",
     height: 200,
     borderRadius: 10,
     marginBottom: 15,
   },
   eventTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
-  eventGenre: {
+  eventDescription: {
     fontSize: 18,
-    color: 'grey',
     marginBottom: 5,
   },
   eventDate: {
-    fontSize: 16,
+    fontSize: 14,
+    color: "grey",
     marginBottom: 5,
   },
   eventLocation: {
-    fontSize: 16,
+    fontSize: 14,
+    color: "grey",
     marginBottom: 20,
   },
   closeButton: {
-    backgroundColor: 'orange',
+    backgroundColor: "orange",
     padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   closeButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
