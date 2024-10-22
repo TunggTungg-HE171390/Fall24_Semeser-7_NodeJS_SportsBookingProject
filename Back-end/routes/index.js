489ae345e079597fac4f23c22f58b8d4e638b12b
Anh179005 <anhnvvhe179005@fpt.edu.vn>
2024-10-22 22:14:14 +0700

    fix be

---
+++ Back-end/routes/index.js
@@ -0,0 +1,9 @@
+const FieldRouter = require("./field.route");
+const PostRouter = require("./post.route");
+const UserRouter = require("./user.route");
+
+module.exports = {
+  FieldRouter,
+  PostRouter,
+  UserRouter,
+};
