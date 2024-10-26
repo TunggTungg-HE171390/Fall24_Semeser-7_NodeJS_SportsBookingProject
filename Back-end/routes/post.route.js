ed659814c7846f9438ba97c2c37acad8a432592e
TunggTungg-HE171390 <tungtxhe171390@fpt.edu.vn>
2024-10-27 01:03:31 +0700

    merge main 27/10

--- Back-end/routes/post.route.js
+++ Back-end/routes/post.route.js
@@ -6,12 +6,10 @@ router.use(bodyParser.json());
 
 const { PostController } = require("../controllers");
 
-<<<<<<< HEAD
 router.get("/", PostController.getAllPosts);
 router.get("/:id", PostController.getPostById);
 router.post("/", PostController.createPost);
 router.put("/:id", PostController.updatePost);
-=======
 // Get all posts
 router.get("/", PostController.getAllPosts);
 
@@ -25,7 +23,6 @@ router.post("/", PostController.createPost);
 router.put("/:id", PostController.updatePost);
 
 // Upload image
->>>>>>> bc268dc6956cf31aa4c74d71e2fef9d7aa993667
 router.post("/upload", PostController.uploadImage);
 
 module.exports = router;
--- Back-end/routes/user.route.js
+++ Back-end/routes/user.route.js
@@ -6,20 +6,16 @@ router.use(bodyParser.json());
 
 const { UserController } = require("../controllers");
 
-<<<<<<< HEAD
 router.get("/list", UserController.getAllUsers);
 router.get("/userInfo/:id", UserController.getUserById);
 router.post("/create", UserController.createUser);
 router.post("/change-password/:id", UserController.changePassword);
 router.post("/updateInfo/:id", UserController.updateUser);
 router.post("/forgetPassword", UserController.forgotPassword);
-=======
+
 router.get("/", UserController.getAllUsers);
 router.post("/", UserController.createUser);
-<<<<<<< HEAD
-=======
+
 router.post("/login", UserController.login);
->>>>>>> bc268dc6956cf31aa4c74d71e2fef9d7aa993667
->>>>>>> main
 
 module.exports = router;
