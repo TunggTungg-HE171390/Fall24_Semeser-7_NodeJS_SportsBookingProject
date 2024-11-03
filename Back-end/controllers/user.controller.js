d6f78a8c6177e991176e1b4719d3282f4dacbf0c
TunggTungg-HE171390 <tungtxhe171390@fpt.edu.vn>
2024-11-04 05:43:35 +0700

    edit profile

--- Back-end/controllers/user.controller.js
+++ Back-end/controllers/user.controller.js
@@ -126,24 +126,55 @@ const getUserById = async (req, res, next) => {
 
 const updateUser = async (req, res, next) => {
   try {
+    const userId = req.params.id;
     const newPhone = req.body.phone;
-    const newAvatar = req.body.avatar;
+    const newName = req.body.name;
+    console.log(newName);
 
-    const updateInfo = {
-      "profile.phone": newPhone,
-      "profile.avatar": newAvatar,
-    };
+    if (newName !== undefined) {
+      const checkNameExist = await userModel.findOne({ "profile.name": newName, _id: { $ne: userId } }); //bỏ qua user mình
+      if (checkNameExist) {
+        return res.status(400).json({ message: "Name already exists" });
+      }
+    }
+
+    if (newPhone !== undefined) {
+      const checkPhoneExist = await userModel.findOne({ "profile.phone": newPhone, _id: { $ne: userId } });
+      if (checkPhoneExist) {
+        return res.status(400).json({ message: "Phone already exists" });
+      }
+    }
+
+    const updateInfo = {};
+
+    if (newName !== undefined) {
+      updateInfo["profile.name"] = newName;
+    }
+    if (newPhone !== undefined) {
+      updateInfo["profile.phone"] = newPhone;
+    }
+
+    if (Object.keys(updateInfo).length === 0) {
+      return res.status(400).json({ message: "No valid fields to update" });
+    }
 
     const updatedUser = await userModel.findByIdAndUpdate(
-      req.params.id,
+      userId,
       updateInfo,
       { new: true }
     );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     res.status(200).json(updatedUser);
   } catch (error) {
     next(error);
   }
 };
+
+
 const editUserFromAdmin = async (req, res, next) => {
   try {
     const { id, name, phone, role } = req.body;
--- Front-end/components/Tab_Navigator.js
+++ Front-end/components/Tab_Navigator.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Modal, TextInput, Alert, Label } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Profile from '../Manage_History_Booking/Profile';
 import History from '../Manage_History_Booking/History';
@@ -11,6 +11,11 @@ import axios from "axios";
 export default function CustomTabScreen() {
   const [selectedTab, setSelectedTab] = useState('Profile');
   const [count, setCount] = useState(0);
+
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [visible, setVisible] = useState(false);
+
   const userName = useSelector(state => state.auth.user?.name);
   const userId = useSelector(state => state.auth.user?.id);
 
@@ -32,6 +37,29 @@ export default function CustomTabScreen() {
     }
   };
 
+  const userInfoDetail = async () => {
+    try {
+      const res = await axios.get(`http://192.168.1.38:3000/user/userInfo/${userId}`);
+      setName(res.data.profile.name);
+      setPhone(res.data.profile.phone);
+      console.log("User details fetched:", res.data.profile.name, res.data.profile.phone);
+    } catch (error) {
+      console.log("Error fetching user details:", error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const updatedData = { name, phone };
+      await axios.post(`http://192.168.1.38:3000/user/updateInfo/${userId}`, updatedData);
+      console.log("User information updated successfully");
+      Alert.alert("Success", "Update user information successfully");
+      setVisible(false);
+    } catch (error) {
+      console.error("Error updating user information:", error);
+    }
+  };
+
   const renderContent = () => {
     switch (selectedTab) {
       case "Profile":
@@ -47,17 +75,23 @@ export default function CustomTabScreen() {
     }
   };
 
+  const handleEditButtonPress = async () => {
+    await userInfoDetail(); // Gọi hàm để lấy dữ liệu người dùng trước khi mở modal
+    setVisible(true); // Sau khi có dữ liệu, mới mở modal
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <View style={styles.profileContainerInfo}>
-          <Image
-            source={{
-              uri: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQYbPC_y5E_pg3CiD_uFQi2BKqQJJJx04BaHsF1Xlse_W5q_VdL",
-            }}
-            style={styles.profileImage}
-          />
-        </View>
+      <View style={styles.profileContainerInfo}>
+      <Image
+        source={{
+          uri: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQYbPC_y5E_pg3CiD_uFQi2BKqQJJJx04BaHsF1Xlse_W5q_VdL",
+        }}
+        style={styles.profileImage}
+      />
+      <Icon name="camera" size={30} color="black" style={styles.cameraIcon} />
+    </View>
 
         <View style={styles.threadButton}>
           <TouchableOpacity style={{ flexDirection: 'row' }}>
@@ -73,7 +107,7 @@ export default function CustomTabScreen() {
 
       <View style={styles.profileContainerButton}>
         <View style={styles.titleContainer}>
-          <TouchableOpacity style={styles.editButton}>
+          <TouchableOpacity style={styles.editButton} onPress={handleEditButtonPress}>
             <Icon name="edit" size={18} color="#ccc" style={styles.icon} />
             <Text style={styles.buttonText}>Chỉnh sửa trang cá nhân</Text>
           </TouchableOpacity>
@@ -104,11 +138,52 @@ export default function CustomTabScreen() {
           ))}
         </View>
       </View>
-      {/* Tab Content */}
       <View style={styles.component}>{renderContent()}</View>
+
+      <Modal
+        visible={visible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Update User Information</Text>
+            <Text style={styles.label}>Name</Text>
+            <TouchableOpacity style={[styles.inputContainer, { marginBottom: 20 }]}>
+            <TextInput
+                placeholder="Enter Name"
+                value={name}
+                onChangeText={setName}
+                style={styles.input}
+              />
+            </TouchableOpacity>
+            <Text style={styles.label}>Phone</Text>
+            <TouchableOpacity style={styles.inputContainer}>
+              <TextInput
+                placeholder="Enter Phone"
+                value={phone}
+                onChangeText={setPhone}
+                style={styles.input}
+                keyboardType="phone-pad"
+              />
+            </TouchableOpacity>
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.updateButton} onPress={handleUpdate}>
+                <Text style={styles.buttonText}>Update</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.cancelButton} onPress={() => setVisible(false)}>
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -252,9 +327,21 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   profileContainerInfo: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    // width: '98%',
+    position: 'relative',
+    width: 150, 
+    height: 150, 
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 75, 
+    borderColor: '#0000000',
+    borderWidth: '5px'
+  },
+  cameraIcon: {
+    position: 'absolute',
+    bottom: 5,
+    right: 12,
   },
   componentUnder: {
     backgroundColor: "gray",
@@ -271,4 +358,55 @@ const styles = StyleSheet.create({
     marginRight: 22,
     marginBottom: 18,
   },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContainer: {
+    width: 300,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // width: "50%",
+    marginTop: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#333",
+  },
+  updateButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  cancelButton: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 5,
+    marginLeft: 10,
+  },
+  inputContainer: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+  },
+  label:{
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#333",
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+  }
 });
